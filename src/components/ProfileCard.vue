ee09e2b4f88cb7ece1f966f866f70e5541de0c2c
Alexvue <SimCityrulit22@gmail.com>
2020-01-24 16:55:31 +0300

    Create ProfileCard.vue

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="overline mb-4">{{login}}</div>
+        <v-list-item-title class="headline mb-1">{{name}}</v-list-item-title>
+      </v-list-item-content>
+
+      <v-img
+      max-height="80"
+      max-width="80"
+      :src="link"
+      ></v-img>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn text>Перейти в профиль</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+    props:['name', 'login', 'id'],
+    data() {
+        return {
+            link:'https://randomuser.me/api/portraits/men/' + this.id +'.jpg'
+        }
+    },
+}
+</script>
