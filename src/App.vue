6e06b1a09df8cb75b61d7b4f417efa91ffa429bf
Alexvue <SimCityrulit22@gmail.com>
2020-01-24 16:55:24 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,47 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="app">
+    <v-navigation-drawer color="green"
+                         dark
+                         right
+                         app
+                         expand-on-hover
+                         hide-overlay
+                         permanent>
+      <v-list
+          nav 
+          shaped
+          dense>
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src='https://randomuser.me/api/portraits/men/1.jpg' alt="мужчина">
+          </v-list-item-avatar>
+          <v-list-item-content class="text-left">
+            <v-list-item-title class="font-weight-black">SocialLink</v-list-item-title>
+            <v-list-item-subtitle>Kurtis Weissnat</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-divider class="my-3"></v-divider>  
+        <v-list-item link
+                     :to="link.path + '/7'"
+                     :key="link.path + '/7'"
+                     v-for="link in links">
+          <v-list-item-icon>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="text-left">
+              {{link.label}}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-content class="px-12 py-3">
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <style lang="scss">
@@ -13,19 +50,43 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
-
 #nav {
-  padding: 30px;
-
   a {
     font-weight: bold;
-    color: #2c3e50;
-
     &.router-link-exact-active {
       color: #42b983;
     }
   }
 }
 </style>
+<script>
+export default {
+  props: {
+      source: String,
+    },
+  data(){
+    return {
+      drawer: null,
+      profile: [],
+      links: [
+        {
+          label: 'Главная',
+          path: '/',
+          icon: 'mdi-home-outline'
+        },
+        {
+          label: 'Пользователи',
+          path: '/users',
+          icon: 'mdi-fingerprint'
+        },
+        {
+           label: 'Профиль',
+           path: '/profile',
+           icon: 'mdi-face'
+        }
+      ]
+    }
+  }
+}
+</script>
